e1d47fa9bc3a90c39477cb16714e34a7982cba5c
wilkes <wilkesjoiner@gmail.com>
2009-12-21 11:03:05 -0600

    initial setup

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject candygram "0.1.0-SNAPSHOT"
+  :description "A clojure entiy framework for MongoDB"
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [org.mongodb/mongo-java-driver "1.1"]])
