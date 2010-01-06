dc92251ab15c273fe8be3c730e8620c0e833e7f3
wilkes <wilkesjoiner@gmail.com>
2010-01-06 17:58:46 -0600

    fixed :$exists bug, added updating modifiers

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
                  [org.mongodb/mongo-java-driver "1.2.1"]]
-  :dev-dependencies [[swank-clojure "1.1.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure "1.1.0-SNAPSHOT"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]])
