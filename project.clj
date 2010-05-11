308606f6bdeddb2796420a3ac9b9f31353f541c8
wilkes <wilkesjoiner@gmail.com>
2010-05-11 12:21:04 -0500

    updated back 1.2.0-SNAPSHOT's

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject karras "0.2.0-SNAPSHOT"
   :description "A clojure entity framework for MongoDB"
-
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]          
                  [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
                  [org.mongodb/mongo-java-driver "1.4"]]
-  :dev-dependencies [[swank-clojure "1.1.0"]                     
+  :dev-dependencies [[swank-clojure/swank-clojure "1.2.0-SNAPSHOT"]
+                     [leiningen/lein-swank "1.2.0-SNAPSHOT"]
                      [lein-clojars "0.5.0-SNAPSHOT"]
                      [autodoc "0.7.0"]]
   :autodoc {:web-home "http://wilkes.github.com/karras/"})
