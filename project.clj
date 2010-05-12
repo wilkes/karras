f05b065a15208f55d549d11e26a3c240be114c23
wilkes <wilkesjoiner@gmail.com>
2010-05-12 10:15:02 -0500

    bumped to 0.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject karras "0.2.0-SNAPSHOT"
+(defproject karras "0.3.0-SNAPSHOT"
   :description "A clojure entity framework for MongoDB"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]          
                  [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
