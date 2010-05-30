93003cf9561116a409eb354b11922e5f6df5acbb
wilkes <wilkesjoiner@gmail.com>
2010-05-30 11:34:49 -0500

    updated to inflections 0.4

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[clojure "1.2.0-master-SNAPSHOT"]
                  [clojure-contrib "1.2.0-SNAPSHOT"]
                  [org.mongodb/mongo-java-driver "2.0rc3"]
-                 [inflections "0.3"]]  
+                 [inflections "0.4"]]  
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-clojars "0.5.0"]
                      [autodoc "0.7.1"]]
