4c9685eee67effe8dc6a25dcee5ee07fcdc1e2ac
Ben Mabey <ben@benmabey.com>
2010-07-09 14:43:04 -0600

    fully-qualifies the clojure deps in project.clj
    I've found that earlier versions of leinigen do not work properly without doing
    this.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject karras "0.4.0-SNAPSHOT"
   :description "A clojure entity framework for MongoDB"
-  :dependencies [[clojure "1.2.0-master-SNAPSHOT"]
-                 [clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
                  [org.mongodb/mongo-java-driver "2.0rc3"]
                  [inflections "0.4"]]  
   :dev-dependencies [[swank-clojure "1.2.1"]
