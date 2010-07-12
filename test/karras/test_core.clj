c74ad2467365323833bea9d47b07561b629609e9
Wilkes Joiner <wilkesjoiner@gmail.com>
2010-07-12 14:11:12 -0500

    midjed test_core

--- test/karras/test_core.clj
+++ test/karras/test_core.clj
@@ -1,33 +1,33 @@
 (ns karras.test-core
   (:use karras.core
-        clojure.test)
+        clojure.test
+        midje.semi-sweet)
   (:import [com.mongodb BasicDBObject]))
 
 (deftest test-to-dbo
   (testing "primitives"
-    (is (= nil (to-dbo nil)))
-    (is (= 1 (to-dbo 1)))
-    (is (= \a (to-dbo \a)))
-    (is (= "a" (to-dbo "a"))))
+    (expect (to-dbo nil) => nil)
+    (expect (to-dbo 1) => 1)
+    (expect (to-dbo \a) => \a)
+    (expect (to-dbo "a") => "a"))
   (testing "maps"
-    (is (= (doto (BasicDBObject.)
-             (.put "a" 1)
-             (.put "b" 2))
-           (to-dbo {:a 1 :b 2}))))
+    (expect (to-dbo {:a 1 :b 2}) => (doto (BasicDBObject.)
+                                      (.put "a" 1)
+                                      (.put "b" 2))))
   (testing "list"
-    (is (= [1 2] (to-dbo [1 2])))))
+    (expect (to-dbo [1 2]) => [1 2])))
 
 (deftest test-to-clj
   (testing "primitives"
-    (is (= nil (to-clj nil)))
-    (is (= 1 (to-clj 1)))
-    (is (= \a (to-clj \a)))
-    (is (= "a" (to-clj "a"))))
+    (expect (to-clj nil) => nil)
+    (expect (to-clj 1) => 1)
+    (expect (to-clj \a) => \a)
+    (expect (to-clj "a") => "a"))
   (testing "maps"
-    (is (= {:a 1 :b 2}
-           (to-clj (doto (BasicDBObject.)
-                     (.put "a" 1)
-                     (.put "b" 2))))))
+    (expect (to-clj (doto (BasicDBObject.)
+                      (.put "a" 1)
+                      (.put "b" 2)))
+            => {:a 1 :b 2}))
   (testing "list"
-    (is (= [1 2] (to-clj [1 2])))))
+    (expect (to-clj [1 2]) => [1 2])))
 
